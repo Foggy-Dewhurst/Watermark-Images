77e8da6f7d7737a72283b0d8d920d354df597650
Foggy-Dewhurst <tony.dewhurst@outlook.com>
2024-06-11 17:13:17 +0100

    Add files via upload
    Prerequisite PowerShell Module:   Install-Module ImagePlayGround -Force -Verbose 
    
    Module Usage:- https://github.com/EvotecIT/ImagePlayground

---
+++ watermark.ps1
@@ -0,0 +1,22 @@
+﻿# Directory containing the PNG files
+$SourceDirectory = "B:\AI\webui_forge_cu121_torch21\webui\output\extras-images\New folder\"
+# Directory to save the watermarked images
+$DestinationDirectory = "B:\AI\webui_forge_cu121_torch21\webui\output\extras-images\New folder\watermark\output\"
+
+# Ensure the destination directory exists
+if (!(Test-Path -Path $DestinationDirectory)) {
+    New-Item -ItemType Directory -Path $DestinationDirectory | Out-Null
+}
+
+# Get all PNG files in the source directory
+$FilePaths = Get-ChildItem -Path $SourceDirectory -Filter *.png
+
+foreach ($FilePath in $FilePaths) {
+    # Add watermark
+    $Image = Get-Image -FilePath $FilePath.FullName
+    $Image.WatermarkImage("B:\AI\webui_forge_cu121_torch21\webui\output\extras-images\New folder\watermark\aishorts.png", [ImagePlayground.Image+WatermarkPlacement]::Bottomright, 0.6, 7)
+    
+    # Save image with original name plus -WM
+    $OutputPath = Join-Path -Path $DestinationDirectory -ChildPath ($FilePath.BaseName + "-WM" + $FilePath.Extension)
+    Save-Image -Image $Image -Open -FilePath $OutputPath
+}
